b2193e55ffd1325fb2a3a5abcf963e16277a6f76
azul915 <k.aoyagi940516@gmail.com>
2022-06-16 20:46:02 +0900

    copy raw

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,39 @@
-console.log("Chrome Extension go")
+const copyToClipboard = (tab, text) => {
+    function injectedFunction(text) {
+        try {
+            navigator.clipboard.writeText(text);
+            //console.log('successfully');
+        } catch (e) {
+            //console.log('failed');
+        }
+    }
+    chrome.scripting.executeScript({
+        target: {
+            tabId: tab.id
+        },
+        func: injectedFunction,
+        args: [text]
+    });
+};
+
+const updateContextMenus = async () => {
+    await chrome.contextMenus.removeAll();
+
+    chrome.contextMenus.create({
+        id: "github-copy-raw",
+        title: "Copy Raw GitHub URL",
+        contexts: ["all"]
+    });
+};
+
+chrome.runtime.onInstalled.addListener(updateContextMenus);
+chrome.runtime.onStartup.addListener(updateContextMenus);
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+    switch (info.menuItemId) {
+        case 'github-copy-raw':
+            const targetUrl = tab.url;
+            const rawUrl = targetUrl.replace(/github.com/g, 'raw.githubusercontent.com').replace(/\/blob/g, '');
+            copyToClipboard(tab, rawUrl);
+            break;
+    }
+});
